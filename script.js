2ed0918d07a4d3119d2c247f178762b38e4d523a
Montu12345 <48189882+Montu12345@users.noreply.github.com>
2020-08-10 02:03:34 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+/* global createCanvas, colorMode, HSB, background, height, width, random, 
+           noStroke, fill, ellipse, windowWidth, windowHeight, HSL  */
+
+let dot1;
+
+function setup() {
+  createCanvas(windowWidth - 20, windowHeight - 20);
+  colorMode(HSL, 360, 100, 100);
+  numberOfBalls = 100;
+  balls = [];
+  for (let i = 0 ; i < numberOfBalls; i++){
+      let name = new BouncyDot();
+      balls.push(name)
+    }
+}
+
+function draw() {
+  background(220, 0, 80);
+  
+  for (let i = 0 ; i < balls.length; i++){
+    balls[i].float();
+    balls[i].display();
+    }
+  }
+  
+
+function mousePressed() {
+  // We'll use this for console log statements only.
+  console.log(dot1.x);
+}
+
+class BouncyDot {
+  constructor() {
+    // Randomly generate position
+    this.x = random(width);
+    this.y = random(height);
+    // Randomly generate radius
+    this.r = random(5, 12);
+    // Randomly generate color
+    this.color = random(360);
+    // Randomly generate a master velocity (broken into components)...
+    this.masterXvelocity = random(0.5, 3);
+    this.masterYvelocity = random(0.5, 3);
+    // ...and use those as starting velocities.
+    this.xVelocity = this.masterXvelocity;
+    this.yVelocity = this.masterYvelocity;
+  }
+
+  float() {
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+    // Standard bounce code - like the DVD logo, but for spheres.
+    if (this.x + this.r > width) {
+      this.xVelocity = -1 * this.masterXvelocity;
+    }
+    if (this.x - this.r < 0) {
+      this.xVelocity = this.masterXvelocity;
+    }
+    if (this.y + this.r > height) {
+      this.yVelocity = -1 * this.masterYvelocity;
+    }
+    if (this.y - this.r < 0) {
+      this.yVelocity = this.masterYvelocity;
+    }
+  }
+
+  display() {
+    fill(this.color, 80, 70);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
